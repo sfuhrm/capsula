08b2cf5326232c2f8f6a9443611fc9af2fa66ed0
Stephan Fuhrmann <s@sfuhrm.de>
2017-10-22 23:32:12 +0200

    Added setters

--- src/main/java/de/sfuhrm/capsula/yaml/command/TargetCommand.java
+++ src/main/java/de/sfuhrm/capsula/yaml/command/TargetCommand.java
@@ -21,21 +21,36 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * A comand having a source and target file.
  * @author Stephan Fuhrmann
  */
 public class TargetCommand {
-    @Getter @NotNull
+    @Getter @Setter @NotNull
     private String to;
     
-    @Getter @Size(min = 1)
+    @Getter @Setter @Size(min = 1)
     private String owner;
     
-    @Getter @Size(min = 1)
+    @Getter @Setter @Size(min = 1)
     private String group;
     
-    @Getter @Pattern(regexp = "([r-][w-][x-]){3}") @NotNull
+    @Getter @Setter @Pattern(regexp = "([r-][w-][x-]){3}") @NotNull
     private String mode;
+    
+    public String getOctal() {
+        String myMode = getMode();
+        int value = 0;
+        for (int i = 0; i < 3; i++) {
+            int offset = 3 * i;
+            for (int j = 0; j < 3; j++) {
+                char c = myMode.charAt(offset + j);
+                int bitValue = 2 - j;
+                value |= c != '-' ? (1<<bitValue) << (3*(2-i)) : 0;
+            }
+        }
+        return String.format("%04o", value);
+    }
 }
