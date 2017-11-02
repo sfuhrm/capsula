5a11663df0602787c3c6a2f7984b51fe0996895c
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-02 16:23:17 +0100

    Javadoc

--- src/main/java/de/sfuhrm/capsula/TargetLocator.java
+++ src/main/java/de/sfuhrm/capsula/TargetLocator.java
@@ -68,6 +68,7 @@ public final class TargetLocator {
      * Extracts the target folder from the JAR archive to a temporary file on
      * disk.
      *
+     * @param tempParent the directory to create the temporary files in.
      * @param target the target name to extract.
      * @return the name of the temporary directory where the target was
      * extracted to.
