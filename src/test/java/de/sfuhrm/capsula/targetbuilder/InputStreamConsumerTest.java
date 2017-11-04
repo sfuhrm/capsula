93d144ba36862eca0b90293a9087046f8b4f6300
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-04 10:53:25 +0100

    New test

---
+++ src/test/java/de/sfuhrm/capsula/targetbuilder/InputStreamConsumerTest.java
@@ -0,0 +1,30 @@
+package de.sfuhrm.capsula.targetbuilder;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class InputStreamConsumerTest {
+    @Test
+    public void testRun() {
+        final List<String> lines = new ArrayList<>();
+        Charset charset = Charset.forName("UTF-8");
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(
+                "Foo\nbar\nbaz\n".getBytes(charset));
+        InputStreamConsumer inputStreamConsumer = new InputStreamConsumer(
+                inputStream, s -> lines.add(s), charset);
+        inputStreamConsumer.run();
+
+        assertEquals(
+                Arrays.asList("Foo", "bar", "baz"),
+                lines);
+    }
+}
