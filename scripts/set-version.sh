81a2e30d8914883c188cbb78e8f3053b7cd59022
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-05 16:23:54 +0100

    Script for new version string setting Fix travis yml

---
+++ scripts/set-version.sh
@@ -0,0 +1,23 @@
+NEWVERSION=$1
+
+if [ "x$NEWVERSION" = "x" ]; then
+	echo "Please give a version as a parameter, for example 0.1.4-SNAPSHOT"
+	exit 10
+fi
+
+ROOT=$PWD
+TMP=/tmp/set-version.$$
+
+echo "- .travis.yml"
+sed -e"s/\(.* VERSION: \).*/\1$NEWVERSION/" < $ROOT/.travis.yml > $TMP || exit
+cp $TMP $ROOT/.travis.yml || exit
+
+echo "- pom.xml"
+mvn versions:set -DnewVersion=$NEWVERSION || exit
+
+echo "- capsula.yaml"
+sed -e"s#\(.*from: target/capsula-\).*\(-jar-with-dependencies.jar.*\)#\1$NEWVERSION\2#" < $ROOT/capsula.yaml > $TMP || exit
+cp $TMP $ROOT/capsula.yaml || exit
+
+rm $TMP
+
