6b68b91175fd85121a3badf51c93bd297381c4aa
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-05 18:35:12 +0100

    Added package info

---
+++ src/main/java/de/sfuhrm/capsula/yaml/command/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * YAML to Java POJO command projections.
+ * The objects are used in the target layout file and the
+ * package descriptor file.
+ * @author Stephan Fuhrmann
+ */
+package de.sfuhrm.capsula.yaml.command;
