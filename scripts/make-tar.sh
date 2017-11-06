3c1c3ec8d2a0e89b3aaa3f928a0b630de8b57ea2
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-06 12:30:12 +0100

    Make a tar.gz archive

---
+++ scripts/make-tar.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#
+# Make tar archive of a certain version.
+# Call it like this:
+# set-version.sh 1.0.0-SNAPSHOT
+#
+# (c) 2017 Stephan Fuhrmann
+
+VERSION=$1
+PROJECT=capsula
+
+if [ "x${VERSION}" = "x" ]; then
+	echo "Please give a version as a parameter, for example 0.1.4-SNAPSHOT"
+	exit 10
+fi
+
+ROOT=${PWD}
+TMP=/tmp/${PROJECT}-${VERSION}
+
+mkdir -p ${TMP}
+cp -ar * ${TMP}
+rm -fr ${TMP}/.git*
+rm -fr ${TMP}/target
+rm -fr ${TMP}/build
+rm -fr ${TMP}/out
+rm -f ${TMP}/*.iml
+rm -f ${TMP}/nb*.xml
+rm -f ${TMP}/pom.xml.versionsBackup
+
+cd /tmp; tar -czvf /tmp/${PROJECT}-${VERSION}.tar.gz ${PROJECT}-${VERSION}
+gpg --sign --detach-sign --armor --local-user 0AC5A45E91FA93DA25380017B0D87B063EAD41F1 /tmp/${PROJECT}-${VERSION}.tar.gz
+
+#rm -f ${TMP}
+
