0f5f95ecad91444882aee005d8474d09d621eaaa
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-08 00:27:24 +0100

    Checkstyle

--- src/main/java/de/sfuhrm/capsula/yaml/Relation.java
+++ src/main/java/de/sfuhrm/capsula/yaml/Relation.java
@@ -19,7 +19,6 @@ package de.sfuhrm.capsula.yaml;
 
 import java.util.Objects;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
